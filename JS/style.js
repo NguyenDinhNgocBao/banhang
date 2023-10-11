b656599827f9075fa5c9c5476a3589f667bb6e0d
LAPTOP T&T <21t1020237@husc.edu.vn>
2023-10-11 21:38:39 +0700

    push

--- JS/style.js
+++ JS/style.js
@@ -18,3 +18,20 @@ function showFrom(){
     })
 }   
 showFrom()
+
+
+
+// Lấy tham số truy vấn từ URL
+const urlParams = new URLSearchParams(window.location.search);
+const productName = urlParams.get('name');
+const productPrice = urlParams.get('price');
+const productImage = urlParams.get('image');
+
+// Hiển thị thông tin sản phẩm trên trang
+const productImageElement = document.getElementById('product-image');
+const productNameElement = document.getElementById('product-name');
+const productPriceElement = document.getElementById('product-price');
+
+productImageElement.src = productImage;
+productNameElement.textContent = productName;
+productPriceElement.textContent = `$${productPrice}`;
